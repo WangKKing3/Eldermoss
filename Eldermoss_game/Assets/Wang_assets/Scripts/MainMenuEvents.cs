5aeafb9cc0e1bdd442cbf29141d21c7dd67652e4
Wang King <99259940+WangKKing3@users.noreply.github.com>
2025-11-06 10:47:44 +0100

    Script
    Add button sound and clikk registration on Play button

---
+++ Eldermoss_game/Assets/Wang_assets/Scripts/MainMenuEvents.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+
+public class MainManuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+    private Button _button;
+
+    private List<Button> _menuButtons = new List<Button>();
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>(); 
+
+        _document = GetComponent<UIDocument>();
+        _button = _document.rootVisualElement.Q("StartGameButton") as Button;
+        _button.RegisterCallbackOnce<ClickEvent>(OnPlayGameClick);
+
+        _menuButtons = _document.rootVisualElement.Query<Button>().ToList();
+
+        for (int i = 0; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
+        }
+    }
+
+    private void OnDnable()
+    {
+        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+
+        for (int i = 1; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].UnregisterCallback<ClickEvent>(OnAllButtonsClick);
+        }
+
+
+    }
+
+
+    private void OnAllButtonsClick(ClickEvent evt)
+    {
+        Debug.Log("Button Clicked");
+    }
+
+    private void OnPlayGameClick(ClickEvent evt)
+    {
+        if (_audioSource != null)
+        {
+            _audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource component is missing!");
+        }
+    }
+}
