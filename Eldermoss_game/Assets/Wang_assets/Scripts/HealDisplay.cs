db0360bca1a38aafd810b625bea220b2c22f94dc
Wang King <99259940+WangKKing3@users.noreply.github.com>
2025-11-25 15:08:36 +0100

    Health bare sprits

--- Eldermoss_game/Assets/Wang_assets/Scripts/HealDisplay.cs
+++ Eldermoss_game/Assets/Wang_assets/Scripts/HealDisplay.cs
@@ -1,44 +1,46 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class HealDisplay : MonoBehaviour
 {
-
-
     [Header("UI References")]
-    public Sprite emptyHeart;
-    public Sprite fullHeart;
-    public Image[] hearts;
-
+    // Vi endrer disse til arrays ([]) slik at vi kan legge inn mange forskjellige bilder
+    public Sprite[] emptyRunes;
+    public Sprite[] fullRunes;
+    public Image[] hearts;      // Dette er selve Image-objektene i scenen
 
     [Header("Player Reference")]
     public Health playerHealth;
 
-
-    // Update is called once per frame
     void Update()
     {
-
-
         if (playerHealth == null) return;
 
-      
         int currentHealth = playerHealth.CurrentLives;
         int maxHealth = playerHealth.MaxLives;
 
         for (int i = 0; i < hearts.Length; i++)
         {
-            if (i < currentHealth)
+            // Sjekk om vi har nok bilder i listene våre for å unngå feil
+            if (i < fullRunes.Length && i < emptyRunes.Length)
             {
-                hearts[i].sprite = fullHeart;
-            }
-            else
-            {
-                hearts[i].sprite = emptyHeart;
+                if (i < currentHealth)
+                {
+                    // Spilleren har liv her: Vis den spesifikke FULL-runen for denne plassen
+                    hearts[i].sprite = fullRunes[i];
+                    hearts[i].enabled = true; // Sørg for at den synes
+                }
+                else
+                {
+                    // Spilleren har mistet dette livet: Vis den spesifikke TOM-runen
+                    hearts[i].sprite = emptyRunes[i];
+
+                    // Hvis du vil at den tomme runen skal være litt gjennomsiktig, kan du fjerne kommentaren under:
+                    // hearts[i].color = new Color(1, 1, 1, 0.5f); 
+                }
             }
 
+            // Håndterer antall beholdere totalt (Max Health)
             if (i < maxHealth)
             {
                 hearts[i].enabled = true;
