fc7e748b5fdb9a1256b064270486a6bfc033d18c
Wang King <99259940+WangKKing3@users.noreply.github.com>
2025-11-25 18:53:04 +0100

    fixit

--- Eldermoss_game/Assets/Oske_assets/transitions/scripts/connection_maker.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "transitions/Level_connections")]
-public class Level_connections : ScriptableObject
-{
-    public static Level_connections ActiveConnection { get; set; }
-}
